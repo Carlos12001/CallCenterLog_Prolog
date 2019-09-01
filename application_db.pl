23af3d7d17905708f84acd8d3f316fac7f31bc97
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-08-31 18:08:42 -0600

    [ADD] Setup Project

---
+++ application_db.pl
@@ -0,0 +1,36 @@
+/**
+ * @file application_db.pl
+ * @version 0.1
+ * @authors angelortizv, isolis2000, jesquivel48
+ * @brief
+ */
+
+% Palabras Clave de Usuario ----------------------------------------------
+
+% Inicio de una Conversación
+saludo([hola|S],S).
+saludo([saludos|S],S).
+saludo([disculpe|S],S).
+saludo([buenos|S],S).
+saludo([buenas|S],S).
+saludo([dias|S],S).
+saludo([tardes|S],S).
+saludo([noches|S],S).
+
+% Fin de una Conversación
+despedida([gracias|S],S).
+despedida([adios|S],S).
+despedida([hasta_luego|S],S).
+despedida([chao|S],S).
+
+% Palabras Clave para el BNF ---------------------------------------------
+
+% determinantes
+determinante([el|S],S).
+determinante([lo|S],S).
+determinante([la|S],S).
+determinante([las|S],S).
+determinante([los|S],S).
+determinante([me|S],S).
+determinante([un|S],S).
+determinante([una|S],S).
---
+++ main.pl
@@ -0,0 +1,8 @@
+/**
+ * @file main.pl
+ * @version 0.1
+ * @authors angelortizv, isolis2000, jesquivel48
+ * @brief
+ */
+
+:-consult('application_db').

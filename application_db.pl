6c2f156e77bc7a1f2a78a08bd086cc4d05b455a1
Iván <isolis@solavcr.com>
2019-08-31 23:12:42 -0600

    [ADD] DB

--- application_db.pl
+++ application_db.pl
@@ -23,32 +23,62 @@ despedida([adios|S],S).
 despedida([hasta_luego|S],S).
 despedida([chao|S],S).
 
-% Palabras Clave para el BNF ---------------------------------------------
+% Palabras Clave para el BNF --------------------------------------------
 
 % Determinantes masculinos
 determinante_m([el|S],S).
 determinante_m([lo|S],S).
 determinante_m([los|S],S).
-determinante_m([me|S],S).
 determinante_m([un|S],S).
+determinante_m([unos|S],S).
+determinante_m([este|S],S).
+determinante_m([estos|S],S).
+determinante_m([nuestro|S],S).
+determinante_m([otro|S],S).
+determinante_m([alguno|S],S).
+determinante_m([algunos|S],S).
 
 % Determinantes femeninos
 determinante_f([la|S],S).
 determinante_f([las|S],S).
 determinante_f([una|S],S).
+determinante_f([unas|S],S).
+determinante_f([esta|S],S).
+determinante_f([estas|S],S).
+determinante_f([nuestra|S],S).
+determinante_f([otra|S],S).
+determinante_f([alguna|S],S).
+determinante_f([algunas|S],S).
+
+% Determinantes neutros
+determinante_n([mi|S],S).
+determinante_n([mis|S],S).
 
 % Sustantivos masculinos
-sustantivo_m([computador|S], S).
+sustantivo_m([computador|S],S).
+sustantivo_m([cable|S],S).
+sustantivo_m([internet|S],S).
+sustantivo_m([cargador|S],S).
+sustantivo_m([ventilador|S],S).
 
 % Sustantivos femeninos
-sustantivo_f([computadora|S], S).
-sustantivo_f([impresora|S], S).
+sustantivo_f([computadora|S],S).
+sustantivo_f([impresora|S],S).
+sustantivo_f([luz|S],S).
 
 % Nombres
 nombre([testNombre|S],S).
 
 % Verbos
-verbo([sirve|S], S).
-verbo([descompuso|S], S).
-verbo([rompio|S], S).
+verbo([sirve|S],S).
+verbo([descompuso|S],S).
+verbo([rompio|S],S).
+
+
+
+
+
+
+% palabra([me|S],S).
+
 
--- main.pl
+++ main.pl
@@ -10,25 +10,36 @@
 
 % BNF --------------------------------------------------------------------
 
-oracion(A,B):- sintagma_nominal(A,C),
-               sintagma_verbal(C,B).
+oracion(A,B):-
+	sintagma_nominal(A,C),
+	sintagma_verbal(C,B).
 
-sintagma_nominal(A, B) :-
-	determinante_m(A, C),
-	sustantivo_m(C, B).
-sintagma_nominal(A, B) :-
-	determinante_f(A, C),
-	sustantivo_f(C, B).
+sintagma_nominal(A,B):-
+	determinante_m(A,C),
+	sustantivo_m(C,B).
+sintagma_nominal(A,B):-
+	determinante_f(A,C),
+	sustantivo_f(C,B).
+sintagma_nominal(A,B):-
+	determinante_n(A,C),
+	sustantivo_f(C,B).
+sintagma_nominal(A,B):-
+	determinante_n(A,C),
+	sustantivo_m(C,B).
 
-sintagma_verbal(A,B):- verbo(A,B).
-sintagma_verbal(A,B):- verbo(A,C),
-                       sintagma_nominal(C,B).
+sintagma_verbal(A,B):-
+	verbo(A,B).
+sintagma_verbal(A,B):-
+	verbo(A,C),
+	sintagma_nominal(C,B).
 
-sintagma_saludo(A,B):- saludo(A,C),
-                       nombre(C,B).
-sintagma_saludo(A,B):- saludo(A,C),
-                       saludo(C,D),
-                       nombre(D,B).
+sintagma_saludo(A,B):-
+	saludo(A,C),
+	nombre(C,B).
+sintagma_saludo(A,B):-
+	saludo(A,C),
+	saludo(C,D),
+	nombre(D,B).
 
 % BNF ------------------------------------------------------------------
 

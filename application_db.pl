b70a357f5ace19b55458cc313c3c48bd195b978e
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-09-05 16:50:17 -0600

    [UPDATE] Decision Tree

--- application_db.pl
+++ application_db.pl
@@ -17,11 +17,12 @@ saludo([buenas,noches|S],S).
 
 % Fin de una Conversacion
 despedida([gracias|S],S).
+despedida([muchas,gracias|S],S).
 despedida([adios|S],S).
 despedida([hasta_luego|S],S).
 despedida([chao|S],S).
 
-% Nombre del Programa
+% nombre del Programa
 nombrePrograma([callCenterLog|S],S).
 nombrePrograma([log|S],S).
 nombrePrograma([callCenter|S],S).
@@ -57,12 +58,12 @@ determinante_f([algunas|S],S).
 determinante_n([mi|S],S).
 determinante_n([mis|S],S).
 
-% Sustantivos masculinos
+% sustantivos masculinos
 sustantivo_m([internet|S],S).
 sustantivo_m([celular|S],S).
 sustantivo_m([televisor|S],S).
 
-% Sustantivos femeninos
+% sustantivos femeninos
 sustantivo_f([computadora|S],S).
 sustantivo_f([impresora|S],S).
 
@@ -70,6 +71,7 @@ sustantivo_f([impresora|S],S).
 verbo([sirve|S],S).
 verbo([me,sirve|S],S).
 verbo([funciona|S],S).
+verbo([funcione|S],S).
 verbo([se,descompuso|S],S).
 verbo([se,rompio|S],S).
 verbo([enciende|S],S).
@@ -87,178 +89,180 @@ verbo([remover|S],S).
 negacion([no|S],S).
 negacion([nunca|S],S).
 
-
 % Lista de Problemas,Causas,Soluciones, Preguntas y Referencias -----------------------------------
 
 % Lista de Problemas, los problemas son tratados como strings
 
-problema("La computadora no enciende").
-problema("No puedo ver mis archivos").
-problema("La impresora no imprime").
-problema("La impresora se le atasca el papel").
-problema("No hay conexión a internet").
-problema("El internet está lento").
-problema("El televisor no funciona").
-problema("El televisor tiene mala imagen").
-problema("El celular no enciende").
-problema("El celular se sobrecalienta").
+problema('la computadora no enciende').
+problema('la computadora no muestra mis archivos').
+problema('la impresora no imprime').
+problema('la impresora se le atasca el papel').
+problema('el internet no tiene conexión').
+problema('el internet esta lento').
+problema('el televisor no funciona').
+problema('el televisor tiene mala imagen').
+problema('el celular no enciende').
+problema('el celular se sobrecalienta').
 
-% Lista de Causas para cada problema, El primer string del hecho es la 
+% Lista de Causas para cada problema, el primer string del hecho es la 
 % causa (la cual es propia y unica para cada problema), el segundo
 % string indica a cual problema pertenece dicha causa.
 
 % Causas relacionados a computadora
-causa("Está desconectada la computadora","La computadora no enciende").
-causa("El tomacorriente no funciona","La computadora no enciende").
-causa("Los cables no están bien conectados","La computadora no enciende").
-causa("Los archivos no existen","No puedo ver mis archivos").
-causa("La unidad de almacenamiento no está bien conectada","No puedo ver mis archivos").
-causa("Los controladores necesarios no están instalados","No puedo ver mis archivos").
+causa('esta desconectada la computadora','la computadora no enciende').
+causa('el tomacorriente no funciona','la computadora no enciende').
+causa('los cables no estan bien conectados','la computadora no enciende').
+causa('los archivos no existen','la computadora no muestra mis archivos').
+causa('la unidad de almacenamiento no esta bien conectada','la computadora no muestra mis archivos').
+causa('los controladores necesarios no estan instalados','la computadora no muestra mis archivos').
 
 % Causas relacionados a impresora
-causa("La impresora no tiene papel","La impresora no imprime").
-causa("La impresora no tiene tinta","La impresora no imprime").
-causa("La impresora no está bien configurada","La impresora no imprime").
-causa("El tamaño de papel no es el correcto","La impresora se le atasca el papel").
-causa("El papel no está bien alineado","La impresora se le atasca el papel").
-causa("El número de hojas cargadas en la impresora es menor a la capacidad","La impresora se le atasca el papel").
+causa('la impresora no tiene papel','la impresora no imprime').
+causa('la impresora no tiene tinta','la impresora no imprime').
+causa('la impresora no esta bien configurada','la impresora no imprime').
+causa('el tamaño de papel no es el correcto','la impresora se le atasca el papel').
+causa('el papel no esta bien alineado','la impresora se le atasca el papel').
+causa('el número de hojas cargadas en la impresora es menor a la capacidad','la impresora se le atasca el papel').
 
 % Causas relacionados a internet
-causa("El módem está desconectado","No hay conexión a internet").
-causa("La computadora no tiene habilitada la tarjeta WIFI","No hay conexión a internet").
-causa("La computadora no está conectada por cable","No hay conexión a internet").
-causa("El router no está conectado al módem","No hay conexión a internet").
-causa("Su dispositivo no está cerca del router","El internet está lento").
-causa("El dispositivo tiene muchas aplicaciones corriendo simultáneamente","El internet está lento").
-causa("La capacidad de internet es inferior a 5MB","El internet está lento").
+causa('el módem esta desconectado','el internet no tiene conexión').
+causa('la computadora no tiene habilitada la tarjeta WIFI','el internet no tiene conexión').
+causa('la computadora no esta conectada por cable','el internet no tiene conexión').
+causa('el router no esta conectado al módem','el internet no tiene conexión').
+causa('su dispositivo no esta cerca del router','el internet esta lento').
+causa('el dispositivo tiene muchas aplicaciones corriendo simultáneamente','el internet esta lento').
+causa('la capacidad de internet es inferior a 5MB','el internet esta lento').
 
 % Causas relacionados a televisor
-causa("El televisor no está conectado","El televisor no funciona").
-causa("El control remoto no tiene bateria","El televisor no funciona").
-causa("El interruptor de ahorro de energía del televisor no está encedido","El televisor no funciona").
-causa("El tomacorriente no funciona","El televisor no funciona").
-causa("El televisor no está bien conectado al cable o antena","El televisor tiene mala imagen").
-causa("El televisor no tiene la actualización más reciente","El televisor tiene mala imagen").
-causa("Hay dispositivos cerca que brindan interferencia del televisor","El televisor tiene mala imagen").
+causa('el televisor no esta conectado','el televisor no funciona').
+causa('el control remoto no tiene bateria','el televisor no funciona').
+causa('el interruptor de ahorro de energía del televisor no esta encedido','el televisor no funciona').
+causa('el tomacorriente no funciona','el televisor no funciona').
+causa('el televisor no esta bien conectado al cable o antena','el televisor tiene mala imagen').
+causa('el televisor no tiene la actualización más reciente','el televisor tiene mala imagen').
+causa('hay dispositivos cerca que brindan interferencia del televisor','el televisor tiene mala imagen').
 
 % Causas relacionados a celular
-/*causa("","El celular no enciende").
-causa("","El celular no enciende").
-causa("","El celular no enciende").
-causa("","El celular se sobrecalienta").
-causa("","El celular se sobrecalienta").
-causa("","El celular se sobrecalienta").*/
+/*causa('','el celular no enciende').
+causa('','el celular no enciende').
+causa('','el celular no enciende').
+causa('','el celular se sobrecalienta').
+causa('','el celular se sobrecalienta').
+causa('','el celular se sobrecalienta').*/
 
 
-% Lista de soluciones para cada subproblema, El primer string del hecho es la 
+% Lista de soluciones para cada subproblema, el primer string del hecho es la 
 % solucion el segundo string indica a cual subproblema pertenece dicha solucion.
 
 % Soluciones relacionadas a computadora
-solucion("Conecte la computadora","Está desconectada la computadora").
-solucion("Intente con otro tomacorriente","El tomacorriente no funciona").
-solucion("Conecte bien los cables","Los cables no están bien conectados").
-solucion("Los archivos no existen","Los archivos no existen").
-solucion("Conecte bien la unidad de almacenamiento","La unidad de almacenamiento no está bien conectada").
-solucion("Instale los controladores necesarios","Los controladores necesarios no están instalados").
+solucion('conecte la computadora','esta desconectada la computadora').
+solucion('intente con otro tomacorriente','el tomacorriente no funciona').
+solucion('conecte bien los cables','los cables no estan bien conectados').
+solucion('los archivos no existen','los archivos no existen').
+solucion('conecte bien la unidad de almacenamiento','la unidad de almacenamiento no esta bien conectada').
+solucion('instale los controladores necesarios','los controladores necesarios no estan instalados').
 
 % Soluciones relacionadas a impresora
-/*solucion("","La impresora no imprime").
-solucion("","La impresora no imprime").
-solucion("","La impresora no imprime").
-solucion("","La impresora se le atasca el papel").
-solucion("","La impresora se le atasca el papel").
-solucion("","La impresora se le atasca el papel").*/
+/*solucion('','la impresora no imprime').
+solucion('','la impresora no imprime').
+solucion('','la impresora no imprime').
+solucion('','la impresora se le atasca el papel').
+solucion('','la impresora se le atasca el papel').
+solucion('','la impresora se le atasca el papel').*/
 
 % Soluciones relacionadas a internet
-/*solucion("","No hay conexión a internet").
-solucion("","No hay conexión a internet").
-solucion("","No hay conexión a internet").
-solucion("","No hay conexión a internet").
-solucion("","El internet está lento").
-solucion("","El internet está lento").
-solucion("","El internet está lento").*/
+/*solucion('','el internet no tiene conexión').
+solucion('','el internet no tiene conexión').
+solucion('','el internet no tiene conexión').
+solucion('','el internet no tiene conexión').
+solucion('','el internet esta lento').
+solucion('','el internet esta lento').
+solucion('','el internet esta lento').*/
 
 % Soluciones relacionadas a televisor
-/*solucion("","El televisor no funciona").
-solucion("","El televisor no funciona").
-solucion("","El televisor no funciona").
-solucion("","El televisor no funciona").
-solucion("","El televisor tiene mala imagen").
-solucion("","El televisor tiene mala imagen").
-solucion("","El televisor tiene mala imagen").*/
+/*solucion('','el televisor no funciona').
+solucion('','el televisor no funciona').
+solucion('','el televisor no funciona').
+solucion('','el televisor no funciona').
+solucion('','el televisor tiene mala imagen').
+solucion('','el televisor tiene mala imagen').
+solucion('','el televisor tiene mala imagen').*/
 
 % Soluciones relacionadas a celular
-/*solucion("","El celular no enciende").
-solucion("","El celular no enciende").
-solucion("","El celular no enciende").
-solucion("","El celular se sobrecalienta").
-solucion("","El celular se sobrecalienta").
-solucion("","El celular se sobrecalienta").*/
+/*solucion('','el celular no enciende').
+solucion('','el celular no enciende').
+solucion('','el celular no enciende').
+solucion('','el celular se sobrecalienta').
+solucion('','el celular se sobrecalienta').
+solucion('','el celular se sobrecalienta').*/
 
-% Lista de Preguntas para cada subproblema, El primer string del hecho es la
+% Solución para caso final
+solucion('Este Sistema Experto no puede responder su problema, se recomienda consultar a un técnico','no hay solucion').
+
+% Lista de Preguntas para cada subproblema, el primer string del hecho es la
 % pregunta que el SE realiza, el segundo string indica a cual problema pertenece.
 
 % Preguntas relacionados a computadora
-pregunta("¿Está conectada la computadora?","Está desconectada la computadora").
-pregunta("¿Ha intentado probar en otro enchufe?","El tomacorriente no funciona").
-pregunta("¿Los cables están bien conectados?","Los cables no están bien conectados").
-pregunta("¿Ya se aseguró de que el archivo que está buscando existe?","Los archivos no existen").
-pregunta("¿Ya se aseguró de que la unidad de almacenamiento está bien conectada?","La unidad de almacenamiento no está bien conectada").
-pregunta("¿Ya instaló los controladores necesarios?","Los controladores necesarios no están instalados").
+pregunta('¿reviso que esté conectada la computadora?','esta desconectada la computadora').
+pregunta('¿ha intentado probar en otro enchufe?','el tomacorriente no funciona').
+pregunta('¿los cables estan bien conectados?','los cables no estan bien conectados').
+pregunta('¿ya se aseguró de que el archivo que esta buscando existe?','los archivos no existen').
+pregunta('¿ya se aseguró de que la unidad de almacenamiento esta bien conectada?','la unidad de almacenamiento no esta bien conectada').
+pregunta('¿ya instaló los controladores necesarios?','los controladores necesarios no estan instalados').
 
 % Preguntas relacionadas a impresora
-/*pregunta("","La impresora no imprime").
-pregunta("","La impresora no imprime").
-pregunta("","La impresora no imprime").
-pregunta("","La impresora se le atasca el papel").
-pregunta("","La impresora se le atasca el papel").
-pregunta("","La impresora se le atasca el papel").*/
+/*pregunta('','la impresora no imprime').
+pregunta('','la impresora no imprime').
+pregunta('','la impresora no imprime').
+pregunta('','la impresora se le atasca el papel').
+pregunta('','la impresora se le atasca el papel').
+pregunta('','la impresora se le atasca el papel').*/
 
 % Preguntas relacionadas a internet
-/*pregunta("","No hay conexión a internet").
-pregunta("","No hay conexión a internet").
-pregunta("","No hay conexión a internet").
-pregunta("","No hay conexión a internet").
-pregunta("","El internet está lento").
-pregunta("","El internet está lento").
-pregunta("","El internet está lento").*/
+/*pregunta('','el internet no tiene conexión').
+pregunta('','el internet no tiene conexión').
+pregunta('','el internet no tiene conexión').
+pregunta('','el internet no tiene conexión').
+pregunta('','el internet esta lento').
+pregunta('','el internet esta lento').
+pregunta('','el internet esta lento').*/
 
 % Preguntas relacionadas a televisor
-/*pregunta("","El televisor no funciona").
-pregunta("","El televisor no funciona").
-pregunta("","El televisor no funciona").
-pregunta("","El televisor no funciona").
-pregunta("","El televisor tiene mala imagen").
-pregunta("","El televisor tiene mala imagen").
-pregunta("","El televisor tiene mala imagen").*/
+/*pregunta('','el televisor no funciona').
+pregunta('','el televisor no funciona').
+pregunta('','el televisor no funciona').
+pregunta('','el televisor no funciona').
+pregunta('','el televisor tiene mala imagen').
+pregunta('','el televisor tiene mala imagen').
+pregunta('','el televisor tiene mala imagen').*/
 
 % Preguntas relacionadas a celular
-/*pregunta("","El celular no enciende").
-pregunta("","El celular no enciende").
-pregunta("","El celular no enciende").
-pregunta("","El celular se sobrecalienta").
-pregunta("","El celular se sobrecalienta").
-pregunta("","El celular se sobrecalienta").*/
+/*pregunta('','el celular no enciende').
+pregunta('','el celular no enciende').
+pregunta('','el celular no enciende').
+pregunta('','el celular se sobrecalienta').
+pregunta('','el celular se sobrecalienta').
+pregunta('','el celular se sobrecalienta').*/
 
-% Lista de referencias para cada subproblema, El primer string del hecho es la
+% Lista de referencias para cada subproblema, el primer string del hecho es la
 % referencia, el segundo string indica a cual problema pertenece dicha solución.
 
 % Referencias relacionadas a computadora
-/*referencias("","La computadora no enciende").
-referencia("","No puedo ver mis archivos").*/
+/*referencias('','la computadora no enciende').
+referencia('','la computadora no muestra mis archivos').*/
 
 % Referencias relacionadas a impresora
-/*referencia("","La impresora no imprime").
-referencia("","La impresora se le atasca el papel").*/
+/*referencia('','la impresora no imprime').
+referencia('','la impresora se le atasca el papel').*/
 
 % Referencias relacionadas a internet
-/*referencia("","No hay conexión a internet").
-referencia("","El internet está lento").*/
+/*referencia('','el internet no tiene conexión').
+referencia('','el internet esta lento').*/
 
 % Referencias relacionadas a televisor
-/*referencia("","El televisor no funciona").
-referencia("","El televisor tiene mala imagen").*/
+/*referencia('','el televisor no funciona').
+referencia('','el televisor tiene mala imagen').*/
 
 % Referencias relacionadas a celular
-/*referencia("","El celular no enciende").
-referencia("","El celular se sobrecalienta").*/
+/*referencia('','el celular no enciende').
+referencia('','el celular se sobrecalienta').*/
--- main.pl
+++ main.pl
@@ -7,6 +7,7 @@
 
 :-consult('application_db').
 :-style_check(-singleton).
+:-dynamic(soluciones/1).
 
 % BNF ---------------------------------------------------------------------------------------------
 
@@ -43,32 +44,29 @@ sintagma_saludo(A,B):-
 
 % BNF ---------------------------------------------------------------------------------------------
 
-inicio():-
-	input_to_list(L),
-	sintagma_saludo(L, []),
-    writeln('Hola usuario'),
-	writeln('¿Cuál es su nombre?'),
-	input_to_string(Nombre),
-	respuesta_saludo(Nombre),
-	validacion_gramatica().
-
-validacion_gramatica():-
+validacion_gramatical():-
 	input_to_list(Oracion),
 	oracion(Oracion,[]),
-	respuesta_validacion_gramatica(), !.
+	% respuesta_validacion_gramatical(), 
+	!.
 
-validacion_gramatica():-
+validacion_gramatical():-
 	writeln('Oración gramaticalmente incorrecta'),nl,
 	writeln('Escriba de nuevo su oración').
-	validacion_gramatica().
+	validacion_gramatical().
 
-respuesta_validacion_gramatica():-
+respuesta_validacion_gramatical():-
 	writeln('Oración gramaticalmente correcta').
 
 respuesta_saludo(Nombre):-
 	write('Hola '),
 	writeln(Nombre),
 	writeln('¿En qué lo puedo ayudar?').
+
+respuesta_despedida():-
+	write('¿Algo más en que pueda servirle?'),
+	input_to_string(Respuesta),
+	fail.
 
 input_to_list(L):-
 	read_line_to_codes(user_input,Cs),
@@ -78,23 +76,27 @@ input_to_string(A):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs).
 
-obtenerCausas(X,A):-
+concatenar([],L,L).
+concatenar([X|L1],L2,[X|L3]):- 
+	concatenar(L1,L2,L3).
+
+eliminar_primeros(L,Y,B):- length(X, B), append(X,Y,L).
+
+obtener_causas(X,A):-
 	split_string(A, "', ,?" ,"', ,?", L),
-	eliminarPrimeros(L,Y,7),
+	eliminar_primeros(L,Y,7),
 	atomic_list_concat(Y, ' ', X),
 	causas(X).
 
-obtenerReferencias(X,A):-
+obtener_referencias(X,A):-
 	split_string(A, "', ,?" ,"', ,?", L),
 	eliminarPrimeros(L,Y,6),
 	atomic_list_concat(Y, ' ', X),
 	referencias(X).
 
-eliminarPrimeros(L,Y,B):- length(X, B), append(X,Y,L).
-
 causas(A):-
 	write('Las principales causas que pueden estar asociadas a: '),
-	write(A), write(" son:"), nl,nl,
+	write(A), write(' son:'), nl,nl,
 	causa(B,A),
 	write(B),nl,
 	fail.
@@ -105,24 +107,86 @@ referencias(A):-
 	write(B),nl,
 	fail.
 
-conversacion():-
-	inicio(),
-	write("Responda con si o no a las siguientes preguntas"),nl,
-	fail.
+raiz('esta desconectada la computadora',computadora):-
+	hoja_izquierda('esta desconectada la computadora'), !.
+raiz('el tomacorriente no funciona',computadora):-
+	hoja_izquierda('el tomacorriente no funciona'), !.
+raiz('los cables no estan bien conectados',computadora):-
+	hoja_izquierda('los cables no estan bien conectados'), !.
+raiz('no hay solucion',computadora):-
+	hoja_izquierda('no hay solucion'), !.
 
-start():-
+hoja_izquierda(B):-
+	soluciones(L),
+	buscar_solucion(B,R,L), !,
+	consulta_general(no,R).
+hoja_izquierda(B):-
+    pregunta(E,B),
+    consulta_no(B, E).
+
+hoja_derecha(B):-
+	soluciones(L),
+	buscar_solucion(B,R,L), !,
+	consulta_general(si,R).
+hoja_derecha(B):-
+    pregunta(E,B),
+    consulta_si(B,E).
+
+consulta_no(A, P):-
+    write(P), nl,
+    read(R), nl,
+    soluciones(L),
+    concatenar(L, [A, R], NL),
+    retractall(soluciones(_)),
+    assert(soluciones(NL)),
+    consulta_general(no, R).
+
+consulta_si(A, P):-
+    write(P), nl,
+    read(R), nl,
+    soluciones(L),
+    concatenar(L, [A, R], NL),
+    retractall(soluciones(_)),
+    assert(soluciones(NL)),
+    consulta_general(si, R).
+	
+consulta_general(R,R).
+
+buscar_solucion(P,R,[P, R|_]).
+buscar_solucion(P,R,[_|C]):-
+    buscar_solucion(P,R,C).
+
+conversacion():-
+	write('Responda con si. o no. a las siguientes preguntas'),nl,nl,
+	retractall(soluciones(_)),
+	assert(soluciones([])),
+	raiz(A,computadora),
+	solucion(B,A),
+	write(B),nl.
+
+inicio():-
 	sleep(0.02),
 		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
 		sleep(0.02),
 		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
 		sleep(0.02),
-		write("|||||||||||||||||||||||| Call Center Log |||||||||||||||||||||||||"),nl,
+		write('|||||||||||||||||||||||| Call Center Log |||||||||||||||||||||||||'),nl,
 		sleep(0.02),
 		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
 		sleep(0.02),
 		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
-	conversacion().	
 
-?- write(" "),nl.
-?- write("Sistema desarrollado por: angelortizv, isolis2000, jesquivel48"),nl.
-?- write("Inserte start() para iniciar el sistema experto."),nl,nl.
+	input_to_list(L),
+	sintagma_saludo(L, []),
+    writeln('Hola usuario'),
+	writeln('¿Cuál es su nombre?'),
+	input_to_string(Nombre),
+	respuesta_saludo(Nombre),
+	validacion_gramatical(),nl,
+	write('Para CallCenterLog es un gusto ayudarle con su problema,'),nl,
+	conversacion(),
+	respuesta_despedida().	
+
+?- write(' '),nl.
+?- write('Sistema desarrollado por: angelortizv, isolis2000, jesquivel48'),nl.
+?- write('Inserte inicio(). para iniciar con el sistema experto.'),nl,nl.

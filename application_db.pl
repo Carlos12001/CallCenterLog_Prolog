fbd24ad992a5bb7cceb4e0c1262d0d079670cba5
Angelo Ortiz Vega <angelo2898@gmail.com>
2019-09-05 01:58:37 -0600

    [UPDATE] Initial Format for application_db

--- application_db.pl
+++ application_db.pl
@@ -5,7 +5,7 @@
  * @brief
  */
 
-% Palabras Clave de Usuario ----------------------------------------------
+% Palabras Clave de Usuario -----------------------------------------------------------------------
 
 % Inicio de una Conversacion
 saludo([hola|S],S).
@@ -15,18 +15,18 @@ saludo([buenos,dias|S],S).
 saludo([buenas,tardes|S],S).
 saludo([buenas,noches|S],S).
 
-% Nombre del Programa
-nombre([callCenterLog|S],S).
-nombre([log|S],S).
-nombre([callCenter|S],S).
-
 % Fin de una Conversacion
 despedida([gracias|S],S).
 despedida([adios|S],S).
 despedida([hasta_luego|S],S).
 despedida([chao|S],S).
 
-% Palabras Clave para el BNF --------------------------------------------
+% Nombre del Programa
+nombrePrograma([callCenterLog|S],S).
+nombrePrograma([log|S],S).
+nombrePrograma([callCenter|S],S).
+
+% Palabras Clave para el BNF ----------------------------------------------------------------------
 
 % Determinantes masculinos
 determinante_m([el|S],S).
@@ -58,20 +58,18 @@ determinante_n([mi|S],S).
 determinante_n([mis|S],S).
 
 % Sustantivos masculinos
-sustantivo_m([computador|S],S).
-sustantivo_m([cable|S],S).
 sustantivo_m([internet|S],S).
-sustantivo_m([cargador|S],S).
-sustantivo_m([ventilador|S],S).
+sustantivo_m([celular|S],S).
+sustantivo_m([televisor|S],S).
 
 % Sustantivos femeninos
 sustantivo_f([computadora|S],S).
 sustantivo_f([impresora|S],S).
-sustantivo_f([luz|S],S).
 
 % Verbos
 verbo([sirve|S],S).
 verbo([me,sirve|S],S).
+verbo([funciona|S],S).
 verbo([se,descompuso|S],S).
 verbo([se,rompio|S],S).
 verbo([enciende|S],S).
@@ -85,11 +83,182 @@ verbo([cambiar|S],S).
 verbo([editar|S],S).
 verbo([remover|S],S).
 
+% Negaciones
+negacion([no|S],S).
+negacion([nunca|S],S).
 
 
+% Lista de Problemas,Causas,Soluciones, Preguntas y Referencias -----------------------------------
+
+% Lista de Problemas, los problemas son tratados como strings
+
+problema("La computadora no enciende").
+problema("No puedo ver mis archivos").
+problema("La impresora no imprime").
+problema("La impresora se le atasca el papel").
+problema("No hay conexión a internet").
+problema("El internet está lento").
+problema("El televisor no funciona").
+problema("El televisor tiene mala imagen").
+problema("El celular no enciende").
+problema("El celular se sobrecalienta").
+
+% Lista de Causas para cada problema, El primer string del hecho es la 
+% causa (la cual es propia y unica para cada problema), el segundo
+% string indica a cual problema pertenece dicha causa.
+
+% Causas relacionados a computadora
+causa("Está desconectada la computadora","La computadora no enciende").
+causa("El tomacorriente no funciona","La computadora no enciende").
+causa("Los cables no están bien conectados","La computadora no enciende").
+causa("Los archivos no existen","No puedo ver mis archivos").
+causa("La unidad de almacenamiento no está bien conectada","No puedo ver mis archivos").
+causa("Los controladores necesarios no están instalados","No puedo ver mis archivos").
+
+% Causas relacionados a impresora
+causa("La impresora no tiene papel","La impresora no imprime").
+causa("La impresora no tiene tinta","La impresora no imprime").
+causa("La impresora no está bien configurada","La impresora no imprime").
+causa("El tamaño de papel no es el correcto","La impresora se le atasca el papel").
+causa("El papel no está bien alineado","La impresora se le atasca el papel").
+causa("El número de hojas cargadas en la impresora es menor a la capacidad","La impresora se le atasca el papel").
+
+% Causas relacionados a internet
+causa("El módem está desconectado","No hay conexión a internet").
+causa("La computadora no tiene habilitada la tarjeta WIFI","No hay conexión a internet").
+causa("La computadora no está conectada por cable","No hay conexión a internet").
+causa("El router no está conectado al módem","No hay conexión a internet").
+causa("Su dispositivo no está cerca del router","El internet está lento").
+causa("El dispositivo tiene muchas aplicaciones corriendo simultáneamente","El internet está lento").
+causa("La capacidad de internet es inferior a 5MB","El internet está lento").
+
+% Causas relacionados a televisor
+causa("El televisor no está conectado","El televisor no funciona").
+causa("El control remoto no tiene bateria","El televisor no funciona").
+causa("El interruptor de ahorro de energía del televisor no está encedido","El televisor no funciona").
+causa("El tomacorriente no funciona","El televisor no funciona").
+causa("El televisor no está bien conectado al cable o antena","El televisor tiene mala imagen").
+causa("El televisor no tiene la actualización más reciente","El televisor tiene mala imagen").
+causa("Hay dispositivos cerca que brindan interferencia del televisor","El televisor tiene mala imagen").
+
+% Causas relacionados a celular
+/*causa("","El celular no enciende").
+causa("","El celular no enciende").
+causa("","El celular no enciende").
+causa("","El celular se sobrecalienta").
+causa("","El celular se sobrecalienta").
+causa("","El celular se sobrecalienta").*/
 
 
+% Lista de soluciones para cada subproblema, El primer string del hecho es la 
+% solucion el segundo string indica a cual subproblema pertenece dicha solucion.
 
-% palabra([me|S],S).
+% Soluciones relacionadas a computadora
+solucion("Conecte la computadora","Está desconectada la computadora").
+solucion("Intente con otro tomacorriente","El tomacorriente no funciona").
+solucion("Conecte bien los cables","Los cables no están bien conectados").
+solucion("Los archivos no existen","Los archivos no existen").
+solucion("Conecte bien la unidad de almacenamiento","La unidad de almacenamiento no está bien conectada").
+solucion("Instale los controladores necesarios","Los controladores necesarios no están instalados").
 
+% Soluciones relacionadas a impresora
+/*solucion("","La impresora no imprime").
+solucion("","La impresora no imprime").
+solucion("","La impresora no imprime").
+solucion("","La impresora se le atasca el papel").
+solucion("","La impresora se le atasca el papel").
+solucion("","La impresora se le atasca el papel").*/
 
+% Soluciones relacionadas a internet
+/*solucion("","No hay conexión a internet").
+solucion("","No hay conexión a internet").
+solucion("","No hay conexión a internet").
+solucion("","No hay conexión a internet").
+solucion("","El internet está lento").
+solucion("","El internet está lento").
+solucion("","El internet está lento").*/
+
+% Soluciones relacionadas a televisor
+/*solucion("","El televisor no funciona").
+solucion("","El televisor no funciona").
+solucion("","El televisor no funciona").
+solucion("","El televisor no funciona").
+solucion("","El televisor tiene mala imagen").
+solucion("","El televisor tiene mala imagen").
+solucion("","El televisor tiene mala imagen").*/
+
+% Soluciones relacionadas a celular
+/*solucion("","El celular no enciende").
+solucion("","El celular no enciende").
+solucion("","El celular no enciende").
+solucion("","El celular se sobrecalienta").
+solucion("","El celular se sobrecalienta").
+solucion("","El celular se sobrecalienta").*/
+
+% Lista de Preguntas para cada subproblema, El primer string del hecho es la
+% pregunta que el SE realiza, el segundo string indica a cual problema pertenece.
+
+% Preguntas relacionados a computadora
+pregunta("¿Está conectada la computadora?","Está desconectada la computadora").
+pregunta("¿Ha intentado probar en otro enchufe?","El tomacorriente no funciona").
+pregunta("¿Los cables están bien conectados?","Los cables no están bien conectados").
+pregunta("¿Ya se aseguró de que el archivo que está buscando existe?","Los archivos no existen").
+pregunta("¿Ya se aseguró de que la unidad de almacenamiento está bien conectada?","La unidad de almacenamiento no está bien conectada").
+pregunta("¿Ya instaló los controladores necesarios?","Los controladores necesarios no están instalados").
+
+% Preguntas relacionadas a impresora
+/*pregunta("","La impresora no imprime").
+pregunta("","La impresora no imprime").
+pregunta("","La impresora no imprime").
+pregunta("","La impresora se le atasca el papel").
+pregunta("","La impresora se le atasca el papel").
+pregunta("","La impresora se le atasca el papel").*/
+
+% Preguntas relacionadas a internet
+/*pregunta("","No hay conexión a internet").
+pregunta("","No hay conexión a internet").
+pregunta("","No hay conexión a internet").
+pregunta("","No hay conexión a internet").
+pregunta("","El internet está lento").
+pregunta("","El internet está lento").
+pregunta("","El internet está lento").*/
+
+% Preguntas relacionadas a televisor
+/*pregunta("","El televisor no funciona").
+pregunta("","El televisor no funciona").
+pregunta("","El televisor no funciona").
+pregunta("","El televisor no funciona").
+pregunta("","El televisor tiene mala imagen").
+pregunta("","El televisor tiene mala imagen").
+pregunta("","El televisor tiene mala imagen").*/
+
+% Preguntas relacionadas a celular
+/*pregunta("","El celular no enciende").
+pregunta("","El celular no enciende").
+pregunta("","El celular no enciende").
+pregunta("","El celular se sobrecalienta").
+pregunta("","El celular se sobrecalienta").
+pregunta("","El celular se sobrecalienta").*/
+
+% Lista de referencias para cada subproblema, El primer string del hecho es la
+% referencia, el segundo string indica a cual problema pertenece dicha solución.
+
+% Referencias relacionadas a computadora
+/*referencias("","La computadora no enciende").
+referencia("","No puedo ver mis archivos").*/
+
+% Referencias relacionadas a impresora
+/*referencia("","La impresora no imprime").
+referencia("","La impresora se le atasca el papel").*/
+
+% Referencias relacionadas a internet
+/*referencia("","No hay conexión a internet").
+referencia("","El internet está lento").*/
+
+% Referencias relacionadas a televisor
+/*referencia("","El televisor no funciona").
+referencia("","El televisor tiene mala imagen").*/
+
+% Referencias relacionadas a celular
+/*referencia("","El celular no enciende").
+referencia("","El celular se sobrecalienta").*/
--- main.pl
+++ main.pl
@@ -6,14 +6,17 @@
  */
 
 :-consult('application_db').
-:- style_check(-singleton).
+:-style_check(-singleton).
 
-
-% BNF --------------------------------------------------------------------
+% BNF ---------------------------------------------------------------------------------------------
 
 oracion(A,B):-
 	sintagma_nominal(A,C),
 	sintagma_verbal(C,B).
+oracion(A,B):-
+	sintagma_nominal(A,C),
+	negacion(C,D),
+	sintagma_verbal(D,B).
 
 sintagma_nominal(A,B):-
 	determinante_m(A,C),
@@ -36,30 +39,90 @@ sintagma_verbal(A,B):-
 
 sintagma_saludo(A,B):-
 	saludo(A,C),
-	nombre(C,B).
+	nombrePrograma(C,B).
 
-% BNF -------------------------------------------------------------------
+% BNF ---------------------------------------------------------------------------------------------
 
 inicio():-
 	input_to_list(L),
 	sintagma_saludo(L, []),
-        writeln('Hola usuario'),
-	writeln('¿Cuál es su nombre?'),
+    writeln('Hola usuario'),
+	writeln('Â¿CuÃ¡l es su nombre?'),
 	input_to_string(Nombre),
-	respuesta_saludo(Nombre).
+	respuesta_saludo(Nombre),
+	validacion_gramatica().
+
+validacion_gramatica():-
+	input_to_list(Oracion),
+	oracion(Oracion,[]),
+	respuesta_validacion_gramatica(), !.
+
+validacion_gramatica():-
+	writeln('OraciÃ³n gramaticalmente incorrecta'),nl,
+	writeln('Escriba de nuevo su oraciÃ³n').
+	validacion_gramatica().
+
+respuesta_validacion_gramatica():-
+	writeln('OraciÃ³n gramaticalmente correcta').
 
 respuesta_saludo(Nombre):-
 	write('Hola '),
 	writeln(Nombre),
-	writeln('¿En qué lo puedo ayudar?').
+	writeln('Â¿En quÃ© lo puedo ayudar?').
 
 input_to_list(L):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs),
 	atomic_list_concat(L,' ',A).
-
 input_to_string(A):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs).
 
+obtenerCausas(X,A):-
+	split_string(A, "', ,?" ,"', ,?", L),
+	eliminarPrimeros(L,Y,7),
+	atomic_list_concat(Y, ' ', X),
+	causas(X).
 
+obtenerReferencias(X,A):-
+	split_string(A, "', ,?" ,"', ,?", L),
+	eliminarPrimeros(L,Y,6),
+	atomic_list_concat(Y, ' ', X),
+	referencias(X).
+
+eliminarPrimeros(L,Y,B):- length(X, B), append(X,Y,L).
+
+causas(A):-
+	write('Las principales causas que pueden estar asociadas a: '),
+	write(A), write(" son:"), nl,nl,
+	causa(B,A),
+	write(B),nl,
+	fail.
+
+referencias(A):-
+	write('Algunas referencias para su problema son: '),nl,
+	causa(B,A),
+	write(B),nl,
+	fail.
+
+conversacion():-
+	inicio(),
+	write("Responda con si o no a las siguientes preguntas"),nl,
+	fail.
+
+start():-
+	sleep(0.02),
+		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
+		sleep(0.02),
+		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
+		sleep(0.02),
+		write("|||||||||||||||||||||||| Call Center Log |||||||||||||||||||||||||"),nl,
+		sleep(0.02),
+		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
+		sleep(0.02),
+		write('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'),nl,
+	conversacion().	
+
+?- write(" "),nl.
+?- write("Sistema desarrollado por: angelortizv, isolis2000, jesquivel48"),nl.
+?- write("Inserte start() para iniciar el sistema experto."),nl,nl.

ce6ce52c1e7b3272cf046f522d5dc20c7b60ffc3
Carlos12001 <carlos.andres12001@gmail.com>
2023-03-21 22:03:30 -0600

    change commet

--- main.pl
+++ main.pl
@@ -1,15 +1,4 @@
-% main.pl -- archivo principal para correr CallCenterLog, contiene reglas de Prolog.
-%
-% Este archivo es parte de  CallCenterLog, El presente tiene como objetivo el desarrollo de una aplicación que se comporte
-% como un experto en la solución de problemas comunes de un Call Center de TI utilizando Prolog. Los Sistemas expertos,
-% de ahora en adelante SE, son aplicaciones de cómputo que involucran experiencia no algorítmica, para resolver cierto
-% tipo de problema. La interfaz debe ser completamente natural utilizando el lenguaje español. El usuario que presenta
-% el problema, ingresa e informa al SE de todos los inconvenientes que tiene (hardware y software) que le impiden realizar
-% sus tareas normalmente y finalmente puede consultar.
-%
-% Version de Archivo		: 0.1
-% Autores					: GitHub@angelortizv, GitHub@jesquivel48, GitHub@isolis2000
-% Úlitma Modificacion		: 07/09/2019, 16:00, @angelortizv
+% Archivo principal para correr CallCenterLog.
 
 :-consult('application_db').
 :-style_check(-singleton).
